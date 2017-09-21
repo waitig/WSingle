14bfed64e15ae9db11d54d01f0a809308ec5dfbf
waitig <waitig199212@gmail.com>
2017-09-21 22:41:09 +0800

    version 1.01

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@ $waitig_foot_code = waitig_gopt('waitig_foot_code');
         <p>本站所有小说内容来源于互联网，所有章节均由网友上传，转载至本站只为宣传本小说让更多读者欣赏。</p>
         <p>如若发现小说内容有与法律抵触之处或对作品版权有质疑，请发邮件告知本站，立即予以处理</p>
         <p>Copyright &copy; <a href="<?= home_url() ?>"><?= get_bloginfo('name') ?></a> 免费小说在线阅读 Powered By <a
-                    href="https://www.waitig.com" title="WSingle主题">WSingle主题</a></p>
+                    href="https://www.waitig.com" title="WSingle主题">WSingle</a></p>
     </div>
 </div>
 <script type="text/javascript" src="http://libs.baidu.com/jquery/1.9.0/jquery.js"></script>
